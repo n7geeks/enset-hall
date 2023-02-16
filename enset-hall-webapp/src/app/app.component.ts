142116d6b41213667f3c186e61667af5dfbb752d
sharry <y.bensadik@outlook.com>
2023-02-16 21:28:16 +0100

    fix: unsubscribe to open observables

--- enset-hall-webapp/src/app/app.component.ts
+++ enset-hall-webapp/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { TranslateModule } from "@ngx-translate/core";
 import { Select, Store } from "@ngxs/store";
 import { UserPreferencesActions } from "./user-preferences/user-preferences.actions";
 import { UserPreferencesState, UserPreferencesStateModel } from "./user-preferences/user-preferences.state";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { UserPreferencesService } from "./user-preferences/user-preferences.service";
 import {
 	ChangeLanguageWidgetComponent
@@ -27,15 +27,24 @@ import {
 	],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 	constructor(private store: Store, private preferencesService: UserPreferencesService) {}
-	@Select(UserPreferencesState) preferences$!: Observable<UserPreferencesStateModel>;
-	ngOnInit(): void {
+	@Select(UserPreferencesState) preferences$?: Observable<UserPreferencesStateModel>;
+	preferencesSubscription?: Subscription;
+	public ngOnInit(): void {
         this.store.dispatch(new UserPreferencesActions.InitLang());
 		this.store.dispatch(new UserPreferencesActions.InitTheme());
-		this.preferences$.subscribe(preferences => {
-			this.preferencesService.setLang(preferences.lang);
-			this.preferencesService.setTheme(preferences.theme);
-		});
+		if (this.preferences$) {
+			this.preferencesSubscription = this.preferences$
+				.subscribe(preferences => {
+				this.preferencesService.setLang(preferences.lang);
+				this.preferencesService.setTheme(preferences.theme);
+			});
+		}
     }
+	public ngOnDestroy(): void {
+		if (this.preferencesSubscription && !this.preferencesSubscription.closed) {
+			this.preferencesSubscription.unsubscribe();
+		}
+	}
 }

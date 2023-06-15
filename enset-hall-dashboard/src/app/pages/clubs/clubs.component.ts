3fe5cba1cde05b633f425dcf3412d8d7cf82b355
ElhoubeBrahim <brahim.elhoube@gmail.com>
2023-06-15 02:43:50 +0100

    refactor: export redendent clubs columns object

--- enset-hall-dashboard/src/app/pages/clubs/clubs.component.ts
+++ enset-hall-dashboard/src/app/pages/clubs/clubs.component.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
 import { Club } from "../../models/clubs.model";
 import { ClubsService } from "../../services/clubs.service";
-import { User } from "../../models/users.model";
 import { NbDialogService } from "@nebular/theme";
 import { AssignGodfatherModalComponent } from "./assign-godfather-modal/assign-godfather-modal.component";
+import { columns } from "../../shared/clubs-table-columns";
 
 @Component({
   selector: "ngx-clubs",
@@ -11,38 +11,7 @@ import { AssignGodfatherModalComponent } from "./assign-godfather-modal/assign-g
 })
 export class ClubsComponent {
   settings = {
-    columns: {
-      logo: {
-        title: "Logo",
-        type: "html",
-        sort: false,
-        width: "50px",
-        valuePrepareFunction: (logo: string) => {
-          return `<img src="${logo}" width="50" height="50" />`;
-        },
-      },
-      name: {
-        title: "Nom",
-        type: "string",
-        sort: false,
-      },
-      about: {
-        title: "A propos",
-        type: "string",
-        sort: false,
-        valuePrepareFunction: (about: string) => {
-          return about.slice(0, 50) + (about.length > 50 ? " ..." : "");
-        },
-      },
-      godfather: {
-        title: "Parrain",
-        type: "string",
-        sort: false,
-        valuePrepareFunction: (godfather: User) => {
-          return godfather.displayName;
-        },
-      },
-    },
+    columns: columns,
     actions: {
       position: "right",
       add: false,
